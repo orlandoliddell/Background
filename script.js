16ab9ea80ed5c7ede387757291e5cf6177db9b50
filthyningen <malikliddell@gmail.com>
2023-01-11 20:44:56 -0500

    texted out old javascript code

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-var index = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var index = 0;
 //starting point for array
 
-function changeColors(){
-    var colors = [
-        "red","blue", "orange", "yellow", "green", "purple"];
+//function changeColors(){
+//    var colors = [
+//        "red","blue", "orange", "yellow", "green", "purple"];
 //Array of colors        
     
-    document.getElementsByTagName("body")[0].style.background = colors[index++];
+ //   document.getElementsByTagName("body")[0].style.background = colors[index++];
 //Locates the "body" element and determines the array is at 0. Then moves by 1 in 
 //the array each time the function is called.
     
-    if(index > colors.length -1)
-    index = 0;
+ //   if(index > colors.length -1)
+//    index = 0;
 //loops back to the begining of the array once the last color is shown.
-}
+//}
