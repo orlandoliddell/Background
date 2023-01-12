9043f74eaebfc2568c85677feed1d5ef42a60fce
filthyningen <malikliddell@gmail.com>
2023-01-11 21:04:37 -0500

    added hex numbers for colors in an array

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function changeColors(){
+    var hex_numbers =   ["0", "1", "2", "3", "4", "5","6", "7",
+                        "8", "9", "A", "B", "C", "D", "E", "F"];
+    var hexcode = '';
+    
+    
+}
 
 
 
